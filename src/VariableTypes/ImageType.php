d7549f794e99bbb5abf4303167d06aff26701457
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-08-18 09:26:21 +0000

    Fix styling

--- src/VariableTypes/ImageType.php
+++ src/VariableTypes/ImageType.php
@@ -36,15 +36,16 @@ class ImageType implements VariableTypeInterface
 
     public function getValue(mixed $value): mixed
     {
-        if (!is_array($value)) {
+        if (! is_array($value)) {
             return $this->getDefaultValue();
         }
         $alt = $value[current_lang()] ?? $value['alt'] ?? __('Default alt');
-        if (!isset($value['source']) || !isset($value['width']) || !isset($value['height'])) {
+        if (! isset($value['source']) || ! isset($value['width']) || ! isset($value['height'])) {
             return $this->getDefaultValue();
         }
         $value['source'] = asset($value['source']);
         $value['alt'] = $alt;
+
         return $value;
     }
 }
